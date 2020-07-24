ff491dc314616d33d694f17b7afa9f3c07e34cb5
maciejows <owsianny.maciej@gmail.com>
2020-07-24 17:00:51 +0200

    Sdk web playback init

--- src/app/components/app-core/app-core.component.ts
+++ src/app/components/app-core/app-core.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SystemJsNgModuleLoader } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { SpotifyToken } from '../../models/SpotifyToken';
 import { Store } from '@ngrx/store';
+import { get } from 'scriptjs';
 
 @Component({
   selector: 'app-app-core',
@@ -27,6 +28,39 @@ export class AppCoreComponent implements OnInit {
         this.tokenType = token.tokenType;
       }
     )
+    get('https://sdk.scdn.co/spotify-player.js', ()=>{
+      (window as any).onSpotifyWebPlaybackSDKReady = () => {
+        const token = this.tokenValue;
+        // @ts-ignore
+        const player = new Spotify.Player({
+          name: 'Web Playback SDK Quick Start Player',
+          getOAuthToken: cb => { cb(token); }
+        });
+      
+        // Error handling
+        player.addListener('initialization_error', ({ message }) => { console.error(message); });
+        player.addListener('authentication_error', ({ message }) => { console.error(message); });
+        player.addListener('account_error', ({ message }) => { console.error(message); });
+        player.addListener('playback_error', ({ message }) => { console.error(message); });
+      
+        // Playback status updates
+        player.addListener('player_state_changed', state => { console.log(state); });
+      
+        // Ready
+        player.addListener('ready', ({ device_id }) => {
+          console.log('Ready with Device ID', device_id);
+        });
+      
+        // Not Ready
+        player.addListener('not_ready', ({ device_id }) => {
+          console.log('Device ID has gone offline', device_id);
+        });
+      
+        // Connect to the player!
+        player.connect();
+      };
+      
+    });
   }
 
   getData() {
