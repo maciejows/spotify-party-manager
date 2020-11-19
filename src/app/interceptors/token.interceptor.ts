8ee992b8b72792d876b87f5fbc7cd8cb16497cfb
maciejows <owsianny.maciej@gmail.com>
2020-11-19 15:01:18 +0100

    Http interceptor now gets token from app state instead of local storage

--- src/app/interceptors/token.interceptor.ts
+++ src/app/interceptors/token.interceptor.ts
@@ -1,28 +1,41 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { SpotifyToken } from '@models/SpotifyToken';
+import { Observable, Subscription } from 'rxjs';
+import { AuthState } from '@models/AuthState';
+import { Store } from '@ngrx/store';
 import { AuthService } from '@services/auth.service';
 
 @Injectable()
-export class TokenInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService) {}
+export class TokenInterceptor implements HttpInterceptor, OnDestroy {
+  private tokenValue: string;
+  private tokenSub: Subscription;
+  constructor(
+    private store: Store<{ auth: AuthState }>,
+    private authService: AuthService
+  ) {
+    this.tokenSub = this.store
+      .select((state) => state.auth.token.value)
+      .subscribe((token) => (this.tokenValue = token));
+  }
+
+  ngOnDestroy() {
+    this.tokenSub?.unsubscribe();
+  }
 
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    const spotifyToken: SpotifyToken = this.authService.getLocalStorageToken();
+    this.tokenValue = this.tokenValue
+      ? this.tokenValue
+      : this.authService.getLocalStorageToken().value;
     const requestWithToken = request.clone({
-      headers: request.headers.set(
-        'Authorization',
-        `Bearer ${spotifyToken.value}`
-      )
+      headers: request.headers.set('Authorization', `Bearer ${this.tokenValue}`)
     });
     return next.handle(requestWithToken);
   }
