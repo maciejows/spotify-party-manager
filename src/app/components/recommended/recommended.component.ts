848d95693f356107b055d1f872dca3cf1484be74
maciejows <owsianny.maciej@gmail.com>
2020-11-20 13:12:52 +0100

    Added ngx-toastr module

--- src/app/components/recommended/recommended.component.ts
+++ src/app/components/recommended/recommended.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PlayerService } from '@services/player.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-recommended',
@@ -12,11 +13,19 @@ export class RecommendedComponent implements OnInit {
     genres: [],
     tracks: ['7H138s9ZKhVH1cEdsyoqTs', '10DYsZSnLxQ21aEIa5XRK9']
   };
-  constructor(private playerService: PlayerService) {}
+  constructor(
+    private playerService: PlayerService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {}
 
   hehexd(): void {
+    this.toastr.success('Sukces', 'hehe', {
+      closeButton: true,
+      timeOut: 3000,
+      progressBar: true
+    });
     this.playerService
       .getRecommended(this.seeds)
       .subscribe((data) => console.log(data));
